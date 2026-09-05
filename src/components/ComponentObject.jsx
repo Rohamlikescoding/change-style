0e34e1ae0c337f6c0abfead10bc6e23f707a78d6
Rohamlikescoding <rasadi1704@gmail.com>
2026-09-05 20:43:42 +0330

    fixed border bug, and made it ready for git hub deployment

--- src/components/ComponentObject.jsx
+++ src/components/ComponentObject.jsx
@@ -5,11 +5,18 @@ function ComponentObject() {
   console.log(
     `border-${style.borderWidth} ${style.height} ${style.width} border-${style.borderStyle} ${style.color}`,
   );
+  const borderStyleMap = {
+    solid: "border-solid",
+    dashed: "border-dashed",
+    dotted: "border-dotted",
+    double: "border-double",
+    hidden: "border-hidden",
+  };
 
   return (
     <div
       style={{ borderWidth: `${style.borderWidth}px` }}
-      className={`${style.height} ${style.width} border-${style.borderStyle} ${style.color} ${style.borderColor} ${style.borderRedius}`}
+      className={`${style.height} ${style.width} ${borderStyleMap[style.borderStyle]} ${style.color} ${style.borderColor} ${style.borderRedius}`}
     ></div>
   );
 }
